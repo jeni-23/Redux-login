d8388a4a0a7b72f2628281c0e9b9c9b6c3e99fe6
JeniAro <88921564+JeniAro@users.noreply.github.com>
2023-02-09 19:26:37 +0530

    login-using-redux

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react';
+import {useDispatch,useSelector} from 'react-redux';
+import {login} from '../features/userSlice';
+import './Login.css';
+
+const Login=()=>{
+	const dispatch=useDispatch();
+	const [name,setName]=useState("");
+	const [email,setEmail]=useState("");
+	const [password,setPassword]=useState("");
+	const handleSubmit=(e)=>{
+		e.preventDefault();
+		dispatch(login({
+			name:name,
+			email:email,
+			password:password,
+			loggedIn:true,
+		}))
+	}
+	return(
+		<div className='login'>
+			<form className='login_form'onSubmit={(e)=>handleSubmit(e)}>
+			<h1>Login</h1>
+			<input type='name' placeholder='Name' value={name} onChange={(e)=>setName(e.target.value)}/>
+			<input type='email' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+			<input type='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+			<button type='submit' className='submit_btn'>Submit</button>
+				
+			</form>
+		</div>
+
+		)
+}
+export default Login;
